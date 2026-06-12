bb4851b9078357abd4de391e8c5f080e11e12343
Milan Jaros <jar091@vsb.cz>
2026-06-13 00:11:01 +0200

    add GPUJPEG_4444_F32_P0123_LINEAR

--- src/renderengine_tcp.cpp
+++ src/renderengine_tcp.cpp
@@ -1201,7 +1201,7 @@ int TcpConnection::gpujpeg_encode(int width,
 		param_image.pixel_format = GPUJPEG_4444_U16_P0123;
 	}
 	else if (format == 32) {
-		param_image.pixel_format = GPUJPEG_4444_F32_P0123;
+		param_image.pixel_format = GPUJPEG_4444_F32_P0123_LINEAR;
 	}
 	else {
 		printf("gpujpeg_encode: unsupported format [8,16,32] %d\n", format);
@@ -1259,7 +1259,7 @@ int TcpConnection::gpujpeg_decode(int width,
 		gpujpeg_decoder_set_output_format(g_decoder, GPUJPEG_RGB, GPUJPEG_4444_U16_P0123); //GPUJPEG_444_U8_P012A //GPUJPEG_444_U8_P012Z
 	}
 	else if (format == 32) { //FLOAT - RGB
-		gpujpeg_decoder_set_output_format(g_decoder, GPUJPEG_RGB, GPUJPEG_4444_F32_P0123); //GPUJPEG_444_U8_P012Z
+		gpujpeg_decoder_set_output_format(g_decoder, GPUJPEG_RGB, GPUJPEG_4444_F32_P0123_LINEAR); //GPUJPEG_444_U8_P012Z
 	}
 	else {
 		printf("gpujpeg_decode: unsupported format [8,16,32] %d\n", format);
